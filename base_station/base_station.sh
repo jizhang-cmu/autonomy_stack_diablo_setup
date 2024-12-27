b4d732b8069b7dfa200d2400873b2c54d4712fdf
Ji Zhang <zhangji@cmu.edu>
2024-12-27 11:00:43 -0500

    update

--- base_station/base_station.sh
+++ base_station/base_station.sh
@@ -5,6 +5,6 @@ export ROS_DOMAIN_ID=1
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd $SCRIPT_DIR
-ros2 run image_transport republish compressed raw --ros-args --remap in/compressed:=/camera/image/compressed --remap out:=/camera/image/transmitted &
+ros2 run image_transport republish --ros-args -p in_transport:=compressed -p out_transport:=raw --remap in/compressed:=/camera/image/compressed --remap out:=/camera/image/transmitted &
 sleep 1
 ros2 run rviz2 rviz2 -d ./base_station.rviz
